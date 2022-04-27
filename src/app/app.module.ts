98c7d04598a199370b0042a7e716b347a2593861
Emily Tan <emilytanyinyean@gmail.com>
2022-04-27 19:07:27 +0800

    beautify CRUD UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     BrowserModule,
     AppRoutingModule, 
     HttpClientModule,
+    FormsModule,
     ReactiveFormsModule 
   ],
   providers: [ApiService],
